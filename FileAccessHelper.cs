eb7b3ac5703f698020925279aa89091ad13c4693
domGitDev <silvadom2014@gmail.com>
2017-09-20 20:19:53 +0100

    Create FileAccessHelper.cs
    Added FileAccessHelper.cs

---
+++ FileAccessHelper.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace TestApp.iOS
+{
+    class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string filename, bool isDirectory=false)
+        {
+            string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = System.IO.Path.GetFullPath(System.IO.Path.Combine(docFolder, "..", "Library"));
+
+            if (!System.IO.Directory.Exists(libFolder))
+            {
+                System.IO.Directory.CreateDirectory(libFolder);
+            }
+            string filepath = System.IO.Path.Combine(libFolder, filename);
+            if (!System.IO.Directory.Exists(filepath) && isDirectory)
+                    System.IO.Directory.CreateDirectory(filepath);
+            
+            return filepath;
+        }
+    }
+}
